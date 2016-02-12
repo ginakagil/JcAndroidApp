940521541cad031eb5abe63e1710057784cb54a9
ginakagil <ginakagil@gmail.com>
2016-02-12 17:11:09 +0800

    12.02.2016

--- app/src/main/java/com/example/jinkelly/jcandroidapp/ProcessTrans.java
+++ app/src/main/java/com/example/jinkelly/jcandroidapp/ProcessTrans.java
@@ -3,6 +3,7 @@ package com.example.jinkelly.jcandroidapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 /**
  * Created by jin.chung on 11/2/2016.
  */
-public class ProcessTrans extends Activity {
+public class ProcessTrans extends AppCompatActivity {
     public String action_mode = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
